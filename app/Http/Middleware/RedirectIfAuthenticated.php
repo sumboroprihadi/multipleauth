51831296eb37103aad21cf76a6121c2cf3b3511d
Sumboro <sumboro.work@gmail.com>
2019-03-18 18:12:59 +0700

    fix Middleware handle function

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,10 +17,24 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+        switch ($guard) {
+            case 'admin':
+                if (Auth::guard($guard)->check()) {
+                return redirect()->route('admin');
+                }
+                break;
+            default:
+                if (Auth::guard($guard)->check()) {
+                    return redirect('/');
+                }
+                break;
         }
-
         return $next($request);
+
+        // if (Auth::guard($guard)->check()) {
+        //     return redirect('/home');
+        // }
+
+        // return $next($request);
     }
 }
